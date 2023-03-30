9623bd2ba9122149134fd0abf3c96d155b057761
Luigidefilippo <defilippoluigi65@gmail.com>
2023-03-30 22:56:50 +0200

    add methods and finish exercise

--- js/script.js
+++ js/script.js
@@ -23,5 +23,23 @@ createApp({
         }
 
     },
+    methods: {
+      done(index) {
+           if (this.tasks[index].done === false) {
+               this.tasks[index].done = true;
+           } else if (this.tasks[index].done === true) {
+               this.tasks[index].done = false;
+           }
+      },
+
+      deleteTask(index) {
+           this.tasks.splice(index, 1)
+      },
+
+      addNewTask: function() {
+       this.tasks.push({title: this.newTask, done:false});
+       this.newTask = "";
+      }
+   }
     
 }).mount('#app');
